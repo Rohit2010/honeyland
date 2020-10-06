a659f1695551fc2a48391ee772b105e46b1e5392
rohit <rohitdeshmukh2409@gmail.com>
2020-10-06 21:44:57 +0530

    5  commit

--- routes/userdata.js
+++ routes/userdata.js
@@ -10,42 +10,43 @@ router.post("/userdata", (req, res) => {
     const user = new User(req.body);
     console.log(req.body)
     user.save().then((user) => {
-        client.messages
-            .create({
-                body: 'Thank you for filling the form',
-                from: '+12017205829',
-                to: `+91${req.body.number}`
+            client.messages
+                .create({
+                    body: 'Thank you for filling the form',
+                    from: '+12017205829',
+                    to: `+91${req.body.number}`
+                })
+                .then(message => console.log(message.sid));
+            res.json({
+                name: user.name,
+                email: user.email
             })
-            .then(message => console.log(message.sid));
-        res.json({
-            name:user.name,
-            email:user.email
-        })
 
-    })
+        })
         .catch((err) => {
-            return res.json({ msg: "failed to upload data",
-        error:err })
+            return res.json({
+                msg: "failed to upload data",
+                error: err
+            })
         })
 })
 
 
-router.get("/getdata",(req, res) => {
+router.get("/getdata", (req, res) => {
     User.find()
-    .then((user) => {
-        return res.json(user)
-    })
-    .catch(err => console.log(err))
+        .then((user) => {
+            return res.json(user)
+        })
+        .catch(err => console.log(err))
 })
-// router.get("/search",(req, res) => {
+router.get('/user/:id', (req, res) => {
+    User.findById({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            res.status(422).json(err)
+        }
+        res.json(user)
+    })
 
-//     const search = new Search(req.body)
-
-//     User.findOne({number:res.body.number})
-//     .then(user => {
-//         console.log(user)
-//     })
-//     .catch(err => console.log(err))
-// })
+})
 
 module.exports = router;
