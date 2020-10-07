562ea864183211e3b00ad83ca6a1bd02a03e08c6
rohit <rohitdeshmukh2409@gmail.com>
2020-10-07 23:33:13 +0530

    8  commit

--- models/user.js
+++ models/user.js
@@ -2,115 +2,115 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-  const userSchema = new Schema({
-      agentName:{
-          type:String,
-      },
-      name: {
-         type: String,
-         required:true,
-      },
-      email: {
+const userSchema = new Schema({
+    agentName: {
         type: String,
-        required:true,
-        trim:true
-      },
-      number: {
-          type:Number,
-          required:true,
-          trim:true
-      },
-      address:{
-            type:String,
-            required:true
-      },
-      remark:{
-        type:String,
-        default:"no remark"
-      },
-      status:{
-            type:String,
-            default:"Application"
-      },
-      occupation:{
-          type:String,
-          required:true,
-      },
-      city:{
-          type:String,
-          required:true,  
-      },
-      district:{
-          type:String,
-          required:true,  
-      },
-      loanType:{
-            type:String,
-            required:true
-      },
-      photo:{
-          type:String,
-          default:"no photo"
-      },
-      familyIncome: {
-        name:{
-            type:String,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    number: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    remark: {
+        type: String,
+        default: "no remark"
+    },
+    status: {
+        type: String,
+        default: "Application"
+    },
+    occupation: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    district: {
+        type: String,
+        required: true,
+    },
+    loanType: {
+        type: String,
+        required: true
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    familyIncome: {
+        name: {
+            type: String,
         },
-        relation:{
-            type:String,
+        relation: {
+            type: String,
         },
-        occupation:{
-            type:String,
+        occupation: {
+            type: String,
         },
-        income:{
-            type:Number,
+        income: {
+            type: Number,
         }
     },
-      
-      familyIncomeb: {
-        name:{
-            type:String,
+
+    familyIncomeb: {
+        name: {
+            type: String,
         },
-        relation:{
-            type:String,
+        relation: {
+            type: String,
         },
-        occupation:{
-            type:String,
+        occupation: {
+            type: String,
         },
-        income:{
-            type:Number,
+        income: {
+            type: Number,
         }
-      },
-      familyIncomec: {
-        name:{
-            type:String,
+    },
+    familyIncomec: {
+        name: {
+            type: String,
         },
-        relation:{
-            type:String,
+        relation: {
+            type: String,
         },
-        occupation:{
-            type:String,
+        occupation: {
+            type: String,
         },
-        income:{
-            type:Number,
+        income: {
+            type: Number,
         }
-      },
-      familyIncomed: {
-        name:{
-            type:String,
+    },
+    familyIncomed: {
+        name: {
+            type: String,
         },
-        relation:{
-            type:String,
+        relation: {
+            type: String,
         },
-        occupation:{
-            type:String,
+        occupation: {
+            type: String,
         },
-        income:{
-            type:Number,
+        income: {
+            type: Number,
         }
-      }
+    }
 
 
-  })
+})
 
-  module.exports = mongoose.model("User" , userSchema)
+module.exports = mongoose.model("User", userSchema)
--- routes/userdata.js
+++ routes/userdata.js
@@ -5,30 +5,79 @@ const User = require("../models/user")
 const accountSid = 'ACfcfbbdb92f711e6de7cca317dc29fde9';
 const authToken = '3120398d9b856b6296c7fd38197c029a';
 const client = require('twilio')(accountSid, authToken);
+const _ = require("lodash");
+const fs = require("fs");
+
 router.post("/userdata", (req, res) => {
 
-    const user = new User(req.body);
-    console.log(req.body)
-    user.save().then((user) => {
-            client.messages
-                .create({
-                    body: 'thank you',
-                    from: '+12017205829',
-                    to: `+91${req.body.number}`
-                })
-                .then(message => console.log(message.sid));
-            res.json({
-                name: user.name,
-                email: user.email
+    // const user = new User(req.body);
+    // console.log(req.body)
+    // user.save().then((user) => {
+    //         client.messages
+    //             .create({
+    //                 body: 'thank you',
+    //                 from: '+12017205829',
+    //                 to: `+91${req.body.number}`
+    //             })
+    //             .then(message => console.log(message.sid));
+    //         res.json({
+    //             name: user.name,
+    //             email: user.email
+    //         })
+
+    //     })
+    //     .catch((err) => {
+    //         return res.json({
+    //             msg: "failed to upload data",
+    //             error: err
+    //         })
+    //     })
+    let form = new formidable.IncomingForm();
+
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "problem with image"
+            })
+        }
+
+
+        //destructure the fields
+        const { name, email, number, address, occupation, city, district, loanType } = fields;
+
+        if (!name || !email || !number || !address || !occupation || !city || !district || !loanType) {
+            return res.json({
+                error: "All fields are required"
             })
 
+        }
+
+        let user = new User(fields)
+
+        //handle file here
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "picture is so big"
+                })
+            }
+            user.photo.data = fs.readFileSync(file.photo.path);
+            user.photo.contentType = file.photo.type;
+        }
+        //save to the db
+        user.save((err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "saving user in db is failed"
+                })
+            }
+            res.json(user)
         })
-        .catch((err) => {
-            return res.json({
-                msg: "failed to upload data",
-                error: err
-            })
-        })
+
+
+    })
 })
 
 
